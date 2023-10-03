78ba36cfc1136d1f69e05dafaec9a3b763b55d83
tunggary <gsy1656@gmail.com>
2023-10-03 21:19:02 +0900

    jdbc - JDBC를 이용한 데이터 수정

--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -6,17 +6,24 @@ import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
+
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
     @Test
     void crud() throws SQLException {
         //create
-        Member member = new Member("memberV1", 1000);
+        Member member = new Member("memberV2", 1000);
         repository.save(member);
 
         //read
         Member findMember = repository.findById(member.getMemberId());
-        Assertions.assertThat(findMember).isEqualTo(member); //@Data 내부에 @EqualsAndHashCode 있어서 비교 가능
+        assertThat(findMember).isEqualTo(member); //@Data 내부에 @EqualsAndHashCode 있어서 비교 가능
+
+        //update: money: 10000 -> 20000
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
     }
 }
