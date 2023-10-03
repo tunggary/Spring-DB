4e19aa9c361f41d80d9cacb7fca967eca5d2b7ad
tunggary <gsy1656@gmail.com>
2023-10-03 21:13:51 +0900

    jdbc - JDBC를 이용한 데이터 조회

--- jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -6,18 +6,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 @Slf4j
 @Repository
 public class MemberRepositoryV0 {
     public Member save(Member member) throws SQLException {
         String sql = "insert into member(member_id, money) values (?, ?)";
-        Connection connection = null;
+        Connection con = null;
         PreparedStatement pstmt = null;
 
         try {
-            connection = getConnection();
-            pstmt = connection.prepareStatement(sql);
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
             pstmt.setString(1, member.getMemberId());
             pstmt.setInt(2, member.getMoney());
             pstmt.executeUpdate();
@@ -25,12 +26,38 @@ public class MemberRepositoryV0 {
         } catch (SQLException e) {
             throw e;
         } finally {
-            close(connection, pstmt, null);
+            close(con, pstmt, null);
         }
-
     }
 
-    public void close(Connection con, Statement st, ResultSet rs) {
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId = "+ memberId);
+            }
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
+        }
+    }
+
+    private void close(Connection con, Statement st, ResultSet rs) {
         if (con != null) {
             try{
                 con.close();
--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,6 +1,7 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
@@ -9,8 +10,13 @@ class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
     @Test
-    void save() throws SQLException {
-        Member member = new Member("id1", 1000);
+    void crud() throws SQLException {
+        //create
+        Member member = new Member("memberV1", 1000);
         repository.save(member);
+
+        //read
+        Member findMember = repository.findById(member.getMemberId());
+        Assertions.assertThat(findMember).isEqualTo(member); //@Data 내부에 @EqualsAndHashCode 있어서 비교 가능
     }
 }
