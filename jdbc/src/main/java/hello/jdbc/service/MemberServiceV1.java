936951f5a7d7d965bcfbd2df7f05aa84de718eee
tunggary <gsy1656@gmail.com>
2023-10-07 15:05:15 +0900

    transaction - V1 계좌 이체 서비스 로직 추가

---
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV1.java
@@ -0,0 +1,28 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceV1 {
+    private final MemberRepositoryV1 memberRepository;
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromMember.getMemberId(), fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toMember.getMemberId(), toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId() == "ex") {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
