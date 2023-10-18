423a4abe51f334299411bfa164c40d129d53b108
tunggary <gsy1656@gmail.com>
2023-10-18 17:23:43 +0900

    spring exception - V4 MemberRepository 인터페이스 적용 및 SQLException 제거를 통한 예외 누수 문제 해결

---
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV4.java
@@ -0,0 +1,40 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepository;
+import hello.jdbc.repository.MemberRepositoryV3;
+import hello.jdbc.repository.MemberRepositoryV4_1;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberServiceV4 {
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) {
+        //비즈니스 로직
+        bizLogic(fromId, toId, money);
+    }
+
+    private void bizLogic(String fromId, String toId, int money) {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromMember.getMemberId(), fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toMember.getMemberId(), toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId() == "ex") {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
